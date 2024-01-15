dfe0b876b25d736ef127eebe65a0f0dcbac04c51
Ubuntu <azureuser@KF7029DLT.cdztr4dxxnbubhpwgcnoitffih.ax.internal.cloudapp.net>
2024-01-15 10:09:19 +0000

    chaincode for DLT proof of concept

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const landRegistry = require('./lib/LandRegistry');
+
+module.exports.LandRegistry = landRegistry;
+
+module.exports.contracts = [landRegistry];
